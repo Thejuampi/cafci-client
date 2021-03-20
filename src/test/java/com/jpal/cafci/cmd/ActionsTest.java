d4fe2d66296b645079015a1c8a49c58a6c4af3a0
Juan Lescano <juanpablo.lescano@gmail.com>
2021-03-20 13:59:44 -0300

    changed api -> test fix

--- src/test/java/com/jpal/cafci/cmd/ActionsTest.java
+++ src/test/java/com/jpal/cafci/cmd/ActionsTest.java
@@ -1,25 +1,24 @@
 package com.jpal.cafci.cmd;
 
-import com.jpal.cafci.shared.Result;
 import org.junit.jupiter.api.Test;
 
 import static com.jpal.cafci.shared.Result.ok;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ActionsTest {
 
     @Test
     void file_action() {
-        assertEquals(ok(new ReadFileAction()), Actions.actionOrError("file"));
+        assertEquals(ok(new ReadFileAction()), Actions.v2("file"));
     }
 
     @Test
     void fund_action() {
-        assertEquals(ok(new FundAction("arg")), Actions.actionOrError("fund arg"));
+        assertEquals(ok(new FundAction("arg")), Actions.v2("fund --name arg"));
     }
 
     @Test
     void fetch_action() {
-        assertEquals(ok(new FetchFundsAction()), Actions.actionOrError("fetch"));
+        assertEquals(ok(new FetchFundsAction()), Actions.v2("fetch"));
     }
 }
